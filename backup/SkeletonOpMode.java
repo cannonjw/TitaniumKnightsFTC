a2d40bfdc7f2a56197dcfb2419eb5f4eb518d3d1
Jon. Cannon <cannonjw@gmail.com>
2021-01-05 21:02:47 -0700

    Adding backup from 2019

---
+++ backup/SkeletonOpMode.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by beepboopbeep on 12/13/17.
+ */
+
+@TeleOp(name="Hyper", group="TestTeleOp")
+@Disabled
+//Add "//" to @Disabled to Enable Op Mode
+public class SkeletonOpMode extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+    @Override
+    public void stop () {
+
+    }
+}
+
